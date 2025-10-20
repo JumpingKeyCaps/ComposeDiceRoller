b5cb1788ef0ca9de4be1771495e504fcc635623a
Samuel Lebailly <sam.meusa@gmail.com>
2025-10-20 16:41:42 +0200

    feature(ui/composition): Add the Vec3 function to manipulate rotation, normalization,cross,dot ... (all the geometry stuff there!)

---
+++ app/src/main/java/com/lebaillyapp/composediceroller/ui/composition/SpinningCube.kt
@@ -0,0 +1,60 @@
+package com.lebaillyapp.composediceroller.ui.composition
+
+import androidx.compose.ui.geometry.Offset
+import kotlin.math.*
+
+data class Vec3(val x: Float, val y: Float, val z: Float) {
+    operator fun plus(other: Vec3) = Vec3(x + other.x, y + other.y, z + other.z)
+    operator fun minus(other: Vec3) = Vec3(x - other.x, y - other.y, z - other.z)
+    operator fun times(scalar: Float) = Vec3(x * scalar, y * scalar, z * scalar)
+
+    fun rotateX(angle: Float): Vec3 {
+        val cos = cos(angle)
+        val sin = sin(angle)
+        return Vec3(x, y * cos - z * sin, y * sin + z * cos)
+    }
+
+    fun rotateY(angle: Float): Vec3 {
+        val cos = cos(angle)
+        val sin = sin(angle)
+        return Vec3(x * cos + z * sin, y, -x * sin + z * cos)
+    }
+
+    fun cross(other: Vec3) = Vec3(
+        y * other.z - z * other.y,
+        z * other.x - x * other.z,
+        x * other.y - y * other.x
+    )
+
+    fun normalize(): Vec3 {
+        val len = sqrt(x * x + y * y + z * z)
+        return if (len > 0) Vec3(x / len, y / len, z / len) else this
+    }
+
+    fun dot(other: Vec3) = x * other.x + y * other.y + z * other.z
+}
+
+// Helper to avoid zero length vector
+fun Offset.normalizeOrZero(): Offset {
+    val len = hypot(this.x, this.y)
+    return if (len > 0f) Offset(this.x / len, this.y / len) else Offset.Zero
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
