22a99a03e2b1a66949964a5a8a12d7747caef69e
Samuel Lebailly <sam.meusa@gmail.com>
2025-10-20 17:10:01 +0200

    feature(model): Move data class to the model package.

---
+++ app/src/main/java/com/lebaillyapp/composediceroller/model/RotationState.kt
@@ -0,0 +1,7 @@
+package com.lebaillyapp.composediceroller.model
+
+// Classe interne pour gérer l'état de rotation actuel d'un cube
+class RotationState(
+    var rotX: Float = 0f,
+    var rotY: Float = 0f
+)
