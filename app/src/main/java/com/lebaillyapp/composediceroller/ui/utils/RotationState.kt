4a7e5d31252ec0f1870a3ae29d86c13e73c3e926
Samuel Lebailly <sam.meusa@gmail.com>
2025-10-20 16:42:59 +0200

    feature(ui/utils): Create a data model to manage rotation state of the cube.

---
+++ app/src/main/java/com/lebaillyapp/composediceroller/ui/utils/RotationState.kt
@@ -0,0 +1,8 @@
+package com.lebaillyapp.composediceroller.ui.utils
+
+
+// Classe interne pour gérer l'état de rotation actuel d'un cube
+class RotationState(
+    var rotX: Float = 0f,
+    var rotY: Float = 0f
+)
