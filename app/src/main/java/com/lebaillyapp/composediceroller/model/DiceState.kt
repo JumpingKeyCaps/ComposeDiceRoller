1a3a6add529612c45c4d012c48bc5373df5e99b0
Samuel Lebailly <sam.meusa@gmail.com>
2025-10-23 18:35:57 +0200

    feature(model/): Remove LANDING state.

--- app/src/main/java/com/lebaillyapp/composediceroller/model/DiceState.kt
+++ app/src/main/java/com/lebaillyapp/composediceroller/model/DiceState.kt
@@ -15,12 +15,5 @@ enum class DiceState {
      * Le dé tourne rapidement dans toutes les directions
      * Interaction utilisateur désactivée
      */
-    ROLLING,
-
-    /**
-     * État d'atterrissage - ralentissement progressif
-     * Le dé ralentit et s'oriente vers la valeur cible
-     * Transition automatique vers LOCKED
-     */
-    LANDING
+    ROLLING
 }
